22861d31fd6e8233f211879c4c3405d6b881c46d
Jørn Skifter Andersen <skifter@bellcom.dk>
2024-10-23 08:31:54 +0200

    Update extract_certs.sh
    Simplifying cert names

--- scripts/extract_certs.sh
+++ scripts/extract_certs.sh
@@ -12,32 +12,32 @@ if [[ -n "$1" ]]; then
 	read -rs IMPORT
 
 	# Generate certs in this order: private key, trust certificate, fullchain, private key without passphrase
-	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -passout pass:"${IMPORT}" -nocerts -out "${PFX_NO_EXT}"_privkey.pem
-	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -clcerts -nokeys -chain -out "${PFX_NO_EXT}"_trustcert.pem
-	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -cacerts -nokeys -chain -out "${PFX_NO_EXT}"_fullchain.pem
-	/usr/bin/openssl rsa -in "${PFX_NO_EXT}"_privkey.pem -passin pass:"$IMPORT" -out "${PFX_NO_EXT}"_nopass-privkey.pem
+	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -passout pass:"${IMPORT}" -nocerts -out privkey.pem
+	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -clcerts -nokeys -chain -out trustcert.pem
+	/usr/bin/openssl pkcs12 -in "${PFX}" -password pass:"${IMPORT}" -cacerts -nokeys -chain -out fullchain.pem
+	/usr/bin/openssl rsa -in privkey.pem -passin pass:"$IMPORT" -out nopass-privkey.pem
 	# RSA private key
-	/usr/bin/openssl pkcs12 -in "${PFX}" -nocerts -nodes -passin pass:"${IMPORT}" | openssl rsa -out "${PFX_NO_EXT}"_RSA_privkey.pem
+	/usr/bin/openssl pkcs12 -in "${PFX}" -nocerts -nodes -passin pass:"${IMPORT}" | openssl rsa -out RSA_privkey.pem
 
 	# Check if any of the files are empty, and if so remove them, as they'd be of no use as empty.
-	if [[ ! -s ${PFX_NO_EXT}_fullchain.pem ]]; then
-		/bin/rm "${PFX_NO_EXT}"_fullchain.pem
+	if [[ ! -s fullchain.pem ]]; then
+		/bin/rm fullchain.pem
 	fi
 
-	if [[ ! -s ${PFX_NO_EXT}_trustcert.pem ]]; then
-		/bin/rm "${PFX_NO_EXT}"_trustcert.pem
+	if [[ ! -s trustcert.pem ]]; then
+		/bin/rm trustcert.pem
 	fi
 
-	if [[ ! -s ${PFX_NO_EXT}_privkey.pem ]]; then
-		/bin/rm "${PFX_NO_EXT}"_privkey.pem
+	if [[ ! -s privkey.pem ]]; then
+		/bin/rm privkey.pem
 	fi
 
-	if [[ ! -s ${PFX_NO_EXT}_nopass-privkey.pem ]]; then
-		/bin/rm "${PFX_NO_EXT}"_nopass-privkey.pem
+	if [[ ! -s nopass-privkey.pem ]]; then
+		/bin/rm nopass-privkey.pem
 	fi
 
-	if [[ ! -s ${PFX_NO_EXT}_RSA_privkey.pem ]]; then
-		/bin/rm "${PFX_NO_EXT}"_RSA_privkey.pem
+	if [[ ! -s RSA_privkey.pem ]]; then
+		/bin/rm RSA_privkey.pem
 	fi
 
 else
