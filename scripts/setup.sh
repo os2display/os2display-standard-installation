a1fad29f0c8bd77ea9d1c87fcf96b3acbae525cd
Jørn Skifter Andersen <skifter@bellcom.dk>
2025-05-02 11:13:27 +0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -24,6 +24,8 @@ sudo apt purge php8.2*
 sudo a2enmod rewrite
 sudo a2enmod ssl
 
+update-alternatives --set php /usr/bin/php8.2
+
 systemctl restart apache2
 chmod u+x /var/www/display/INSTALL.sh
 
